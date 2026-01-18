df05fe2b5348a7d0af0e168542c08c246aef91a2
Hosein <mahrooghimh@gmail.com>
2026-01-18 16:59:17 +0100

    first commit

---
+++ bin/guassian_privacy_tool.dart
@@ -0,0 +1,5 @@
+import 'package:guassian_privacy_tool/guassian_privacy_tool.dart' as guassian_privacy_tool;
+
+void main(List<String> arguments) {
+  print('Hello world: ${guassian_privacy_tool.calculate()}!');
+}
---
+++ test/guassian_privacy_tool_test.dart
@@ -0,0 +1,8 @@
+import 'package:guassian_privacy_tool/guassian_privacy_tool.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
